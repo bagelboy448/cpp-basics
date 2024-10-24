f9e3476ab34d19a20bf02a7182b9bc2ef9a0c178
bagelboy448 <162947056+bagelboy448@users.noreply.github.com>
2024-10-24 14:24:25 -0700

    Update Stack Template.hpp

--- Stack Template/Stack Template.hpp
+++ Stack Template/Stack Template.hpp
@@ -1,75 +1,149 @@
-#pragma once
+	#pragma once
 
-template<class T>
-class Stack {
-	T* arr;
-	unsigned int elements;
-	unsigned int maximumElements;
-public:
-	Stack() {
-		elements = 0;
-		maximumElements = 0;
-		arr = nullptr;
-	}
-	Stack(unsigned int maximumElements) {
-		this->maximumElements = maximumElements;
-		arr = new T[maximumElements];
-		elements = 0;
-	}
-	Stack(Stack &otherStack) {
-		elements = otherStack.Elements();
-		arr = new T[elements];
-		maximumElements = otherStack.MaximumElements();
-		for (int i = 0; i < elements; i++)
-			arr[i] = otherStack.Get(i);
-	}
-	~Stack() {
-		delete[] arr;
-	}
-	bool Empty() {
-		return elements == 0;
-	}
-	unsigned int Elements() {
-		return elements;
-	}
-	unsigned int MaximumElements() {
-		return maximumElements;
-	}
-	unsigned int Size() {
-		return sizeof(arr);
-	}
-	void Resize(unsigned int newMaximum) {
-		T* newArr = new T[newMaximum];
-		int i = 0;
-		while (i < elements && i < newMaximum) {
-			newArr[i] = arr[i];
-			i++;
-		}
-		elements = i;
-		delete[] arr;
-		arr = newArr;
-	}
-	void Push(T data) {
-		if (elements < maximumElements) {
-			arr[elements] = data;
-			elements++;
-		}
-		//else {}
-	}
-	T Pop() {
-		if (!Empty()) {
-			T temp = arr[elements - 1];
-			elements--;
-			return temp;
-		}
-		//else {}
-	}
-	T Get(unsigned int position) {
-		if (!Empty() && position < elements) {
-			return arr[position];
-		}
-		//else{}
-	}
-	void Print() {}
-	void ReversePrint() {}
-};
+//	Stack class template
+//	====================
+	template<class T>
+	class Stack {
+
+//		Private Member Variables
+//		========================
+		T* arr;
+		unsigned int elements;
+		unsigned int maximumElements;
+//		=============================
+
+//	Public Member Functions
+//	=======================
+	public:
+
+//		Zero-parameter constructor
+//		==========================
+		Stack() {
+			elements = 0;
+			maximumElements = 0;
+			arr = nullptr;
+		} // Stack()
+//		============
+
+//		Maximum size constructor
+//		=====================================
+		Stack(unsigned int maximumElements) {
+			this->maximumElements = maximumElements;
+			arr = new T[maximumElements];
+			elements = 0;
+		} // Stack(unsigned int)
+//		========================
+
+//		Copy constructor
+//		==========================
+		Stack(Stack &otherStack) {
+			elements = otherStack.Elements();
+			arr = new T[elements];
+			maximumElements = otherStack.MaximumElements();
+			for (int i = 0; i < elements; i++)
+				arr[i] = otherStack.Get(i);
+		} // Stack(Stack)
+//		=================
+
+//		Destructor
+//		==========
+		~Stack() {
+			delete[] arr;
+		} // ~Stack()
+//		=============
+
+//		Returns true if the stack is empty
+//		==================================
+		bool Empty() {
+			return elements == 0;
+		} // Empty()
+//		============
+
+//		Returns the number of elements in the stack
+//		===========================================
+		unsigned int Elements() {
+			return elements;
+		} // Elements()
+//		===============
+
+//		Returns the maximum number of elements that can be stored in the stack
+//		======================================================================
+		unsigned int MaximumElements() {
+			return maximumElements;
+		} // MaximumElements()
+//		======================
+
+//		Returns the size occupied by the stack in memory
+//		================================================
+		unsigned int Size() {
+			return sizeof(arr);
+		} // Size()
+//		===========
+
+//		Changes the maximum elements that can be stored in the stack
+//		============================================================
+		void Resize(unsigned int newMaximum) {
+			T* newArr = new T[newMaximum];
+			int i = 0;
+//			========================================
+			while (i < elements && i < newMaximum) {
+				newArr[i] = arr[i];
+				i++;
+			} // while
+//			==========
+			elements = i;
+			delete[] arr;
+			arr = newArr;
+		} // Resize(unsigned int)
+//		=========================
+
+//		Pushes an element onto the stack
+//		================================
+		void Push(T data) {
+			if (elements < maximumElements) {
+				arr[elements] = data;
+				elements++;
+			}
+			//else {}
+		} // Push(T)
+//		============
+
+//		Removes an element from the top of the stack and returns the popped element
+//		===========================================================================
+		T Pop() {
+			if (!Empty()) {
+				T temp = arr[elements - 1];
+				elements--;
+				return temp;
+			}
+			//else {}
+		} // Pop()
+//		==========
+
+//		Returns the element at the top of the stack
+//		===========================================
+		T GetTop() {
+			if (!Empty() && position < elements) {
+				return arr[elements - 1];
+			}
+		} // GetTop()
+//		=============
+
+//		Returns the element at a specified position in the stack
+//		========================================================
+		T Get(unsigned int position) {
+			if (!Empty() && position < elements) {
+				return arr[position];
+			}
+			//else{}
+		} // Get()
+//		==========
+
+//		Undefined Member Functions
+//		==========================
+		void Print() {}
+		void ReversePrint() {}
+//		======================
+
+	}; // class Stack<T> {}
+//	=======================
